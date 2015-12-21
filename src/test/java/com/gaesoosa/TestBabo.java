d088ae470c26197eb8a6a12188868698b1aa7137
ave.j <ave.j@daumkakao.com>
2015-12-21 13:30:28 +0900

    test이상해서 다시 만들어 넣음

--- src/test/java/com/gaesoosa/TestBabo.java
+++
@@ -1,7 +0,0 @@
-package com.gaesoosa;
-
-/**
- * Created by jujeong on 2015. 12. 21..
- */
-public class TestBabo {
-}
---
+++ src/test/java/com/gaesoosa/UserMapperTests.java
@@ -0,0 +1,69 @@
+package com.gaesoosa;
+
+import com.gaesoosa.mapper.UserMapper;
+import com.gaesoosa.model.User;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by jujeong on 2015. 12. 14..
+ */
+//@Transactional
+//@Rollback
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = GaesoosaApplication.class)
+@WebAppConfiguration
+public class UserMapperTests {
+    @Autowired
+    private UserMapper userMapper;
+
+    private User user;
+    @Before
+    public void setUp() {
+    }
+
+    @Ignore
+    public void selectUsers() {
+        List<User> users = userMapper.selectList();
+
+        assertThat("user count equal", users.size() == 3);
+    }
+
+    @Ignore
+    public void insertUser() {
+        user = new User();
+        user.setNickname("jun");
+        user.setAddress("seoul");
+        user.setProfileImage(null);
+        user.setAccessKey(null);
+
+        System.out.println("insert start");
+        System.out.println("nickname" + user.getNickname());
+        userMapper.insert(user);
+        System.out.println("insert after");
+
+//		assertThat("success", selectUser());
+    }
+
+    @Test
+    public void updateUser(){
+        user = new User();
+        user.setNickname("jun_update");
+        user.setAddress("sungname");
+        user.setProfileImage(null);
+        user.setAccessKey(null);
+        user.setId(10);
+
+        userMapper.update(user);
+    }
+}
